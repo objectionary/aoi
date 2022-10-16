701a3fb2320d03babd75053eb84c6ea7701f2aab
OlesiaSub <lesya.sub@mail.ru>
2022-10-16 17:13:22 +0300

    configure repo

---
+++ src/main/kotlin/org/objectionary/oi/launch/Launcher.kt
@@ -0,0 +1,7 @@
+package org.objectionary.oi.launch
+
+import org.slf4j.LoggerFactory
+
+private val logger = LoggerFactory.getLogger("org.objectionary.oi.launch.Launcher")
+
+fun launch(path: String) {}
