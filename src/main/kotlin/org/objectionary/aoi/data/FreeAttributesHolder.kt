a69fd3715f8f71f9149e60409635120b735af27d
OlesiaSub <lesya.sub@mail.ru>
2022-10-22 17:56:36 +0300

    remove puzzle

--- src/main/kotlin/org/objectionary/aoi/data/FreeAttributesHolder.kt
+++ src/main/kotlin/org/objectionary/aoi/data/FreeAttributesHolder.kt
@@ -22,19 +22,17 @@ data class FreeAttribute(val name: String, val holderObject: Node) {
     /**
      * Set of the attributes of this free attribute that were applied in the program
      */
-    val appliedAttributes: MutableSet<Attribute> = mutableSetOf()
+    val appliedAttributes: MutableSet<Parameter> = mutableSetOf()
 }
 
 /**
  * Attribute representation.
  *
  * @property name attribute name
- * @todo #11:30min differentiate attributes by the number of parameters passed
- *  & maybe rename these classes
  */
-data class Attribute(val name: String) {
+data class Parameter(val name: String) {
     /**
-     * Free attributes which this attribute uses
+     * Free attributes which this attribute uses (not used yet)
      */
     val parameters: MutableSet<String> = mutableSetOf()
 }
