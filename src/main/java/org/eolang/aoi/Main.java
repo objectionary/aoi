822c80459a2938da9dc3e2860a5a918bd0479c30
Andrey Chernykh <andreychernykh04@gmail.com>
2025-08-15 14:40:06 +0300

    #133: Add Main class with entry point

---
+++ src/main/java/org/eolang/aoi/Main.java
@@ -0,0 +1,11 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 Objectionary.com
+ * SPDX-License-Identifier: MIT
+ */
+package org.eolang.aoi;
+
+public final class Main {
+    public static void main(final String[] args) {
+        System.out.println("Abstract Object inference for EO Programs");
+    }
+}
