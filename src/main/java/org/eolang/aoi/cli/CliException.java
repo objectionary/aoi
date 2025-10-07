08fba7bc01d2d4bb8182550caebe8043aa58dec2
Andrey Chernykh <andreychernykh04@gmail.com>
2025-10-07 14:08:21 +0300

    #152: Add CliException class for handling CLI errors

---
+++ src/main/java/org/eolang/aoi/cli/CliException.java
@@ -0,0 +1,36 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2025 Objectionary.com
+ * SPDX-License-Identifier: MIT
+ */
+package org.eolang.aoi.cli;
+
+/**
+ * A custom runtime exception for command-line interface (CLI) errors.
+ *
+ * @since 0.0.5
+ */
+public final class CliException extends RuntimeException {
+    /**
+     * The serialization version UID.
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Constructor.
+     *
+     * @param message The detail message.
+     */
+    public CliException(final String message) {
+        super(message);
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param message The detail message.
+     * @param cause   The cause of this exception.
+     */
+    public CliException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+}
